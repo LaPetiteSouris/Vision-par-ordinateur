111b8954897a5c348473b521b2a4d70edd682874
LaPetiteSouris <sontunghoang@gmail.com>
2016-03-06 12:24:23 +0100

    update caliberation image

--- calib_camera.m
+++ calib_camera.m
@@ -6,7 +6,7 @@
 %Load image
 clear all
 clc
-ia=imread('check.jpg');
+ia=imread('checkboard.jpg');
 imshow(ia)
 % grand_X=[1 2 0 1;     1 1 0 1;     2 1 0 1;    0 2 1 1;    0 1 1 1;     0 2 2 1];
 grand_X=34*[1 2 0 1;     1 1 0 1;     2 1 0 1;    0 2 1 1;    0 1 1 1;     0 2 2 1 ; 3 0 0 1; 2 2 0 1; 0 0 2 1; 0 3 3 1];
